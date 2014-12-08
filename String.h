600653adb6f1d83c3a3a0a4f29b3e51af097205f
Nieto Vincent <vnieto@bs406-s16-06.insa-lyon.fr>
2014-12-08 17:55:40 +0100

    Good initialisation of the static attribute MAX_SIZE

--- String.h
+++ String.h
@@ -49,6 +49,8 @@ class String
 
     String(int a_size);
 
+    
+
     // =======================================================================
     //                                Destructor
     // =======================================================================
@@ -91,13 +93,13 @@ class String
     {
       printf("%s:%d: error: call to forbidden constructor.\n", __FILE__, __LINE__);
       exit(EXIT_FAILURE);
-    };*/
+    };
 
     String(const String &model)
     {
       printf("%s:%d: error: call to forbidden constructor.\n", __FILE__, __LINE__);
       exit(EXIT_FAILURE);
-    };
+    };*/
 
 
     // =======================================================================
